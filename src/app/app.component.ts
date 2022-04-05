c0937cafbd0e94da8b6e31161ea4182b2e83e802
ksendrlal <tebbyteb@gmail.com>
2022-04-06 00:19:50 +0300

    added content for component - about-carwash and moved the modal window from home to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Router} from "@angular/router";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,13 +8,25 @@ import {Router} from "@angular/router";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  form!: FormGroup;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) {
   }
 
   toPage (link: string) {
     this.router.navigate([link]);
+  }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      phone: new FormControl('', [Validators.required, Validators.minLength(2)])
+    });
+  }
+
+  onSubmit() {
+
   }
 }
